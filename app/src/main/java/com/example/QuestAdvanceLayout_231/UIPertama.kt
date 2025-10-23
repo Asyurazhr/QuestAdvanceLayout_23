63d9dd8abda38e47598c0b538fd7445912400cce
Asyurazhr <Asyuradjola2004@gmail.com>
2025-10-23 10:10:22 +0700

    Menampilkan teks program studi dengan ukuran font besar dan tebal menggunakan string resource.

--- app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
+++ app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
@@ -31,3 +31,6 @@ fun ActivitasPertama(modifier: Modifier){
     Column(modifier = Modifier.padding(top = 100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
