0cc2d6fd909e7c60c1f0961728bb2973f15ff7b5
Asyurazhr <Asyuradjola2004@gmail.com>
2025-10-23 17:44:04 +0700

    Membuat baris yang di dalamnya ada gambar dari file umy, ukurannya 100 dp, dan ada jarak 5 dp di sekelilingnya.

--- app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
+++ app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
@@ -75,6 +75,14 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.Blue
             )){
+            Row() {
+                val gambar = painterResource(id = R.drawable.umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
+                )
+
         Box(modifier = Modifier
             .fillMaxSize()){
             Text(text = stringResource(id = R.string.copy),
