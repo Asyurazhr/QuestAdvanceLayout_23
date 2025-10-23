80a8819a3af2eb4936965b86d0bbba9e05a14cf2
Asyurazhr <Asyuradjola2004@gmail.com>
2025-10-23 10:13:49 +0700

    Membuat baris yang menampilkan gambar berukuran 100dp dengan padding 5dp di sekitar gambar.

--- app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
+++ app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
@@ -43,3 +43,10 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )){
+            Row() {
+                val gambar = painterResource(id = R.drawable.download)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
+                )
