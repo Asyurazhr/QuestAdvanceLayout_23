2775b7cd15155509b890e4c649b0fd8dec87668d
Asyurazhr <Asyuradjola2004@gmail.com>
2025-10-23 17:50:12 +0700

    Menambahkan Space dengan height 5dp

--- app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
+++ app/src/main/java/com/example/QuestAdvanceLayout_231/UIPertama.kt
@@ -105,6 +105,7 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
+        Spacer(modifier = Modifier.height(height = 5.dp))
 
         Box(modifier = Modifier
             .fillMaxSize()){
